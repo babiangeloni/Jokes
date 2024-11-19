02e4638820238cf67954cd5dc96aeaabe3da1fb9
babiangeloni <158472029+babiangeloni@users.noreply.github.com>
2024-11-19 14:47:10 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,11 +11,12 @@ function displayJoke(response) {
 
 function generateJoke(event) {
   event.preventDefault();
+  let apiKey = "48e39taa86b03f2f04997ba1bo53ba44";
   let context =
     "Your are a funny AI Assistant that tells short and sweet jokes. The joke must be provided in HTML format. Example: <p>this is a joke</p>";
-  let prompt = "Generate a short and unique joke a French people. ";
+  let prompt = "Generate a short and unique joke about Germans. ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-let apiKey= 48e39taa86b03f2f04997ba1bo53ba44;
+
   let jokeElement = document.querySelector("#joke");
 
   jokeElement.innerHTML = "Generating a joke for you.. please wait";
